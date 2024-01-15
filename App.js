d7021cea5ef96e5027f889d0277699ea242a47d3
K R VENKATESH <krvenkatesh@Ks-MacBook-Air.local>
2024-01-16 00:11:45 +0530

    added-react-and-parcel

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 
-const heading = React.createElement('h1',{id:"heading"},'This is from React separate script')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const heading = React.createElement('h1',{id:"heading"},'This is from React separate script for me')
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading)
 
